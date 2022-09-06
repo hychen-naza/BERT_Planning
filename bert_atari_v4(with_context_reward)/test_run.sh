9db07f1763933c0bccd0de3deecf7ac3be501a68
Hongyi Chen <59629242+hychen-naza@users.noreply.github.com>
2022-09-06 13:25:58 -0400

    Add files via upload

---
+++ bert_atari_v4(with_context_reward)/test_run.sh
@@ -0,0 +1,14 @@
+# Decision Transformer (DT) reward_conditioned GoToObjMazeS4 GoToObjMazeS5 GoToObjMazeS6 GoToObjMazeS7  GoToObjMazeS4R2 GoToLocalS7N4 GoToLocalS7N5 GoToLocalS8N3 GoToLocalS8N5 GoToLocalS8N7
+# GoToObjMazeS6 GoToObjMazeS7
+#for env in GoToObjMazeS4 GoToObjMazeS5 
+#do
+#    for seed in 123 231 321 132 213
+#    do
+#        python run_dt_atari.py --seed $seed --horizon 10 --context_length 10 --sample_iteration 50 --epochs 200 --env $env --model_type 'naive' --batch_size 64
+#    done
+#done 231 321 132 213 GoToObjMazeS4Close PutNextLocal GoToSeqS5R2
+
+for seed in 123
+do
+    python run_dt_atari.py --seed $seed --horizon 5 --context_length 25 --sample_iteration 10 --epochs 10 --model_type 'reward_conditioned' --num_steps 5000 --num_buffers 50 --game 'Breakout' --batch_size 64
+done
